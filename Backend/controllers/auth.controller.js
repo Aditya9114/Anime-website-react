a435342da46c0bf75da7d7f0f2c6a8cd17b6ed96
Aditya <dhomneaditya2@gmail.com>
2026-03-25 21:20:18 +0530

    Added Login feature and now Password is Hashed before storing in DB

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,46 +1,85 @@
+import { Error } from "mongoose";
 import { User } from "../models/user.models.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 const registerUser = async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
+  try {
+    const { username, email, password } = req.body;
 
-        const existedUser = await User.findOne({
-            $or: [{ username }, { email }]
-        });
+    const existedUser = await User.findOne({
+      $or: [{ username }, { email }],
+    });
 
-        if (existedUser) {
-            throw new ApiError(409, "User with email or username already exists");
-        }
-
-        const user = await User.create({
-            email,
-            password,
-            username,
-            isEmailVerified: false
-        });
-
-        const createdUser = await User.findById(user._id).select(
-            "-password -refreshToken -emailVerificationToken -emailVerificationExpiry"
-        );
-
-        if (!createdUser) {
-            throw new ApiError(500, "Something went wrong while registering the user");
-        }
-
-        return res
-            .status(201)
-            .json(
-                new ApiResponse(
-                    201,
-                    { user: createdUser },
-                    "User registration successful"
-                )
-            );
-    } catch (err) {
-        throw err;
+    if (existedUser) {
+      throw new ApiError(409, "User with email or username already exists");
     }
+
+    const user = await User.create({
+      email,
+      password,
+      username,
+      isEmailVerified: false,
+    });
+
+    const createdUser = await User.findById(user._id).select(
+      "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+    );
+
+    if (!createdUser) {
+      throw new ApiError(
+        500,
+        "Something went wrong while registering the user",
+      );
+    }
+
+    return res
+      .status(201)
+      .json(
+        new ApiResponse(
+          200,
+          { user: createdUser },
+          "User registration successful",
+        ),
+      );
+  } catch (err) {
+    throw err;
+  }
 };
 
-export { registerUser };
+const loginUser = async (req, res) => {
+  try {
+    const { password, email } = req.body;
+    const existedUser = await User.findOne({ email });
+
+    if (!existedUser) {
+      throw new ApiError(404, "User not found");
+    }
+
+    const correct = await existedUser.isPasswordCorrect(password);
+
+    if (!correct) {
+      throw new ApiError(400, "Invalid Credentials");
+    } else {
+      const createdUser = await User.findById(existedUser._id).select(
+        "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+      );
+
+      return res
+        .status(200)
+        .json(
+          new ApiResponse(
+            201,
+            { user: createdUser },
+            "User logged in Successfully",
+          ),
+        );
+    }
+  } catch (err) {
+    throw err;
+  }
+};
+
+export { registerUser, loginUser };
--- Backend/models/user.models.js
+++ Backend/models/user.models.js
@@ -66,4 +66,16 @@ const userSchema = new Schema(
     }
 )
 
+userSchema.pre("save", async function () {
+
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10);
+
+});
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+
 export const User = mongoose.model("User",userSchema)

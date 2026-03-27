82120bfa1cf232a0641d51b32f84eee73be9d183
Aditya <dhomneaditya2@gmail.com>
2026-03-27 21:31:29 +0530

    Wrote Logout function

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -111,4 +111,32 @@ const loginUser = async (req, res) => {
   }
 };
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler(async (req,res) =>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken : ""
+            }
+        },
+        {
+            new: true,
+        }
+    );
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    };
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(
+            new ApiResponse(200, {}, "User logged Out")
+        )
+})
+
+
+export { registerUser, loginUser, logout};

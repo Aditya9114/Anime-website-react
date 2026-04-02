a86d12a9090be1f29c6aa552455a75548e28b203
Aditya <dhomneaditya2@gmail.com>
2026-04-02 22:03:51 +0530

    Created route and controller to get user's fav. anime

--- Backend/src/controllers/favourites.controller.js
+++ Backend/src/controllers/favourites.controller.js
@@ -9,18 +9,33 @@ const addToFavourites = async (req, res) => {
     const user = await User.findByIdAndUpdate(
       req.user._id,
       {
-        $addToSet: { favourites: anime_id }
+        $addToSet: { favourites: anime_id },
       },
-      { new: true }
+      { new: true },
     );
+    console.log(req.user.favourites);
 
-    return res.status(200).json(
-      new ApiResponse(200, user.favourites, "Added to favourites")
-    );
-
+    return res
+      .status(200)
+      .json(new ApiResponse(200, user.favourites, "Added to favourites"));
   } catch (err) {
     throw new ApiError(500, "Failed to add to favourites");
   }
 };
 
-export { addToFavourites }
+const getFav = async(req,res) =>{
+  try{
+    const user = await User.findById(req.user._id);
+    if(!user){
+      throw new ApiError(404,"User not found");
+    }
+    return res
+      .status(200)
+      .json(new ApiResponse(200, user.favourites, "User's Favourite Anime fetched Successfully"))
+  } 
+  catch(err){
+    throw new ApiError(500, "Failed to fetch favourites");
+  }
+}
+
+export { addToFavourites, getFav };
--- Backend/src/routes/favourites.routes.js
+++ Backend/src/routes/favourites.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import {verifyJWT} from "../middlewares/auth.middleware.js"
-import { addToFavourites } from "../controllers/favourites.controller.js";
+import { addToFavourites,getFav } from "../controllers/favourites.controller.js";
 
 const router = Router();
 router.route('/').post(verifyJWT,addToFavourites )
+router.route('/list').get(verifyJWT,getFav)
 export default router;

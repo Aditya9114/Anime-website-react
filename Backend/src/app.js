6d2f522a98eafe3628cb65be06f2380ea96419ce
Aditya <dhomneaditya2@gmail.com>
2026-04-04 22:35:45 +0530

    Changes made in cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173", // your frontend URL
+    origin: ["http://localhost:5173", "https://anime-website-react-nine.vercel.app"],
     credentials: true,
   })
 );

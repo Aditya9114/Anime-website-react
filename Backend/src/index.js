192744ebed1dc8f694b91b94b6d75ebe03779ba0
Aditya <dhomneaditya2@gmail.com>
2026-04-06 20:44:05 +0530

    Made some changes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -6,7 +6,7 @@ import app from './app.js'
 import connectDB from "./db/index.js";
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 connectDB()
 .then(()=>{
     app.listen(port,()=>{

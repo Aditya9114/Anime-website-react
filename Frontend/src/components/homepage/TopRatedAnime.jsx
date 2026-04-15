08a739961af223fa010b11cadedad7de848f168f
Aditya <dhomneaditya2@gmail.com>
2026-04-15 20:24:48 +0530

    Error message added

--- Frontend/src/components/homepage/TopRatedAnime.jsx
+++ Frontend/src/components/homepage/TopRatedAnime.jsx
@@ -5,7 +5,7 @@ import './topratedanime.css'
 
 export function TopRatedAnime({ topAnime }) {
   return (
-    topAnime.length == 0 ? 
+    !topAnime || topAnime.length == 0 ? 
     (
       <div className='loading-error'>
         <h3>Something went wrong while loading the top anime </h3>
